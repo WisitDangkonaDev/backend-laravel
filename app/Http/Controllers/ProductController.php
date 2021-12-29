fc87f028840b31ddade86601c7df6ac5b508e169
WisitDangkonaDev <wisit.bbm21@gmail.com>
2021-12-30 02:40:33 +0700

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // return Product::all();
+        return Product::orderBy('id','desc')->paginate(25);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //check สิทธฺื
+        $user = auth()->user();
+
+        if($user->tokenCan("1")){
+        $request->validate([
+            'name' =>'required|min:$',
+            'slug' =>'required',
+            'price' =>'required'
+        ]);
+
+        return Product::create($request->all());
+    }else{
+        return[
+            'status' => 'Permission denied to create'
+        ];
+    }
+}
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int   $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Product::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+         //check สิทธฺื
+         $user = auth()->user();
+         if($user->tokenCan("1")){
+        $product = Product::find($id);
+        $product->update($request->all());
+
+        return $product;
+         }else{
+            return[
+                'status' => 'Permission denied to create'
+            ];
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+          //check สิทธฺื
+          $user = auth()->user();
+          if($user->tokenCan("1")){
+            return Product::destroy($id);
+    }else{
+        return[
+            'status' => 'Permission denied to create'
+        ];
+    }
+}
+}
